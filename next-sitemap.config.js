9745520682da504a0b1e708d68039a1311e71e5b
parth <parthrk75@gmail.com>
2025-05-01 21:45:11 +0530

    updated code

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.todaygoldprices.org/', // replace with your real domain
+    generateRobotsTxt: true,               // also create a robots.txt file
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateIndexSitemap: true,
+  };
+  
