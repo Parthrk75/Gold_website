e646daf0c604169679847f9432dda7c17429020d
parth <parthrk75@gmail.com>
2025-05-01 21:58:11 +0530

    updated code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.todaygoldprices.org/', // replace with your real domain
-    generateRobotsTxt: true,               // also create a robots.txt file
+    siteUrl: 'https://www.todaygoldprices.org',
+    generateRobotsTxt: true,
     changefreq: 'weekly',
     priority: 0.7,
     sitemapSize: 5000,
     generateIndexSitemap: true,
+  
+    // Manually add blog pages
+    additionalPaths: async (config) => {
+      const slugs = ['blog1', 'blog2', 'blog3', 'blog4', 'blog5', 'blog6', 'blog7'];
+      return slugs.map((slug) => ({
+        loc: `/blog/${slug}`,
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      }));
+    },
   };
   
